0445826e8a83ba6ac9eb2c4df1d934ee1d9d1512
yildirimbk <124491344+yildirimbk@users.noreply.github.com>
2026-05-16 10:34:45 +0300

    Update dataset_prep_all_BSs.py

--- dataset_prep_all_BSs.py
+++ dataset_prep_all_BSs.py
@@ -1,16 +1,11 @@
 #Next 2 lines for running in an interactive window, uncomment them if you run this file partially/fully in an interactive window
 # import sys
 # sys.argv = ['']
-import sys
 import os
 import pickle
 import h5py
 import hdf5storage
 import numpy as np
-import math
-import matplotlib
-from scipy.io import savemat
-import time
 import copy
 from sklearn.model_selection import train_test_split
 
@@ -19,6 +14,13 @@ RUN_TAG = 'O1_60_Lp4_BS18_rows1-2751_TX8x4_RX4x2_sc1'
 INPUT_DIR = 'outputs'
 OUTPUT_DIR = 'outputs'
 os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+training_dataset_percentage = 80
+validation_dataset_percentage = 10
+test_dataset_percentage = 10
+
+assert training_dataset_percentage + validation_dataset_percentage + test_dataset_percentage == 100, \
+    'Train/val/test percentages must sum to 100.'
 
 #Load Parameters (Required to generate channels)
 with open(os.path.join(INPUT_DIR, f'parameters_{RUN_TAG}.pkl'), 'rb') as f:
@@ -50,7 +52,7 @@ with open(os.path.join(INPUT_DIR, f'User_distances_to_{RUN_TAG}.pkl'), 'rb') as 
 user_dists_to_BS = np.concatenate(user_dists_to_BS)
 
 #Load True Channels between Users and BS
-with h5py.File(INPUT_DIR, f'true_channels_{run_tag}.hdf5', 'r') as hdf:
+with h5py.File(os.path.join(INPUT_DIR, f'true_channels_{RUN_TAG}.hdf5'), 'r') as hdf:
     true_channels = hdf['true_channel matrices'] #Shape ((497931, 8, 32, 1) if multiple BSs (#of selected BS, USERS, Rx, Tx antennas)
     true_channels = np.squeeze(true_channels)  # Shape becomes (497931, 8, 32)
     true_channels = true_channels.reshape(-1, true_channels.shape[1], true_channels.shape[2]) # Shape becomes (497931*3, 8, 32)
@@ -77,29 +79,27 @@ rayt_output_to_be_mod = copy.deepcopy(rayt_output)
 
 def update_rayt_output(rayt_output_in):
     # Define the list of keys that need to be updated
-    keys_to_update = ['DoD_phi', 'DoD_theta', 'DoA_phi', 'DoA_theta', 'phase', 'ToA', 'LoS' , 'power']
+    keys_to_update = ['DoD_phi', 'DoD_theta', 'DoA_phi', 'DoA_theta', 'phase', 'ToA', 'LoS', 'power']
     
     for entry in rayt_output_in:
         num_paths = entry['num_paths']
 
-        # For each key, either pad with zeros or truncate to ensure 5 elements
+        # For each key, either pad with zeros or truncate to ensure max_num_paths elements
         for key in keys_to_update:
             # Convert numpy array to list if necessary
             if isinstance(entry[key], np.ndarray):
                 entry[key] = entry[key].tolist()
             
-            current_length = len(entry[key])
-
             if num_paths == 0:
                 entry[key] = [0.] * max_num_paths  # Fill with four 0s if num_paths is 0
 
             else:
                 # Retain original values up to num_paths
                 entry[key] = entry[key][:num_paths]
-                # Pad with zeros to ensure the total length is 4
+                # Pad with zeros to reach max_num_paths
                 list_req = [0.] * (max_num_paths - len(entry[key]))
                 entry[key].extend(list_req)  # Extend the list with the required number of zeros
-                #if num_paths==4:
+                #if num_paths==max_num_paths:
                 #    print(num_paths)
                 #    print(entry[key])
 
@@ -110,16 +110,20 @@ def update_rayt_output(rayt_output_in):
 
 updated_rayt_output = update_rayt_output(rayt_output_to_be_mod)
 
+
 # for i in range(0,len(rayt_output),100):
 #     print('data i:',i)
 #     print('original rayt_output:',rayt_output[i])
 #     print('upd. rayt_output:',updated_rayt_output[i])
 
 ## Select X% of the data
-
 users=np.arange(total_num_users)
-train_idx, temp_idx = train_test_split(users, test_size=0.2, random_state=42)  # 80% training
-val_idx, test_idx = train_test_split(temp_idx, test_size=0.5, random_state=42)   # 10% validation, 10% test
+train_idx, temp_idx = train_test_split(users, test_size=1-(training_dataset_percentage/100), random_state=42)  # Split training and (validation+test) datasets 
+val_idx, test_idx = train_test_split(                                                                          # Split remaining into val/test according to the configured percentages
+    temp_idx,
+    test_size=test_dataset_percentage / (validation_dataset_percentage + test_dataset_percentage),
+    random_state=42
+)
 
 def split_data(data, train_idx, val_idx, test_idx):
     return data[train_idx], data[val_idx], data[test_idx]
@@ -132,74 +136,29 @@ train_user_loc, val_user_loc, test_user_loc = split_data(user_locations, train_i
 train_user_dist, val_user_dist, test_user_dist = split_data(user_dists_to_BS, train_idx, val_idx, test_idx)
 train_channels, val_channels, test_channels = split_data(true_channels, train_idx, val_idx, test_idx)
 
+def fill_dataset_array(rayt, BS_loc, user_loc, user_dist, los, max_num_paths):
+    n = len(rayt)
+    max_length = 2 + 2 + 1 + 1 + 1 + 8*max_num_paths
+    out = np.zeros([n, max_length])
+    for i in range(n):
+        out[i, 0:2] = BS_loc[i, 0:2]
+        out[i, 2:4] = user_loc[i, 0:2]
+        out[i, 4]   = user_dist[i]
+        out[i, 5]   = los[i]
+        out[i, 6]   = rayt[i]['num_paths']
+        base = 7
+        for k_idx, key in enumerate(['DoD_phi', 'DoD_theta', 'DoA_phi', 'DoA_theta',
+                                     'phase', 'ToA', 'power', 'LoS']):
+            out[i, base + k_idx*max_num_paths : base + (k_idx+1)*max_num_paths] = rayt[i][key]
+    return out
 
-# CREATE TRAINING DATASET
-length_tr = len(train_idx)
-max_length=2+2+1+1+1+8*max_num_paths #(BS_loc(2)+user_loc(2)+overall_LoS_st(1)+ distance(1)\
-#paths(1)+dod_phi(5)_dod_theta(4)+doa_phi(4)_doa_theta(4)+\
-#phase(4)+ToA(4)+power(4)_Los(4)= 39
-training_data = np.zeros([length_tr,max_length])
-#Set BS locations #discard position in z
+# CREATE TRAINING, VALIDATION, AND TEST DATASETS
+training_data = fill_dataset_array(train_rayt, train_BS_loc, train_user_loc, train_user_dist, train_LoS, max_num_paths)
+val_data      = fill_dataset_array(val_rayt,   val_BS_loc,   val_user_loc,   val_user_dist,   val_LoS,   max_num_paths)
+test_data     = fill_dataset_array(test_rayt,  test_BS_loc,  test_user_loc,  test_user_dist,  test_LoS,  max_num_paths)
 
-for i in range(int(length_tr)):
-
-    training_data[i,0:2] = train_BS_loc[i,0:2]
-    training_data[i,2:4] = train_user_loc[i,0:2]
-    training_data[i,4] = train_user_dist[i]
-    training_data[i,5] = train_LoS[i]
-    training_data[i,6] = train_rayt[i]['num_paths']
-    training_data[i,7:7+max_num_paths] = train_rayt[i]['DoD_phi']
-    training_data[i,7+max_num_paths:7+2*max_num_paths] = train_rayt[i]['DoD_theta']
-    training_data[i,7+2*max_num_paths:7+3*max_num_paths] = train_rayt[i]['DoA_phi']
-    training_data[i,7+3*max_num_paths:7+4*max_num_paths] = train_rayt[i]['DoA_theta']
-    training_data[i,7+4*max_num_paths:7+5*max_num_paths] = train_rayt[i]['phase']
-    training_data[i,7+5*max_num_paths:7+6*max_num_paths] = train_rayt[i]['ToA']
-    training_data[i,7+6*max_num_paths:7+7*max_num_paths] = train_rayt[i]['power']
-    training_data[i,7+7*max_num_paths:7+8*max_num_paths] = train_rayt[i]['LoS']
-
-# CREATE VALIDATION DATASET
-length_val = len(val_idx)
-val_data = np.zeros([length_val,max_length])
-
-for i in range(int(length_val)):
-
-    val_data[i,0:2] = val_BS_loc[i,0:2]
-    val_data[i,2:4] = val_user_loc[i,0:2]
-    val_data[i,4] = val_user_dist[i]
-    val_data[i,5] = val_LoS[i]
-    val_data[i,6] = val_rayt[i]['num_paths']
-    val_data[i,7:7+max_num_paths] = val_rayt[i]['DoD_phi']
-    val_data[i,7+max_num_paths:7+2*max_num_paths] = val_rayt[i]['DoD_theta']
-    val_data[i,7+2*max_num_paths:7+3*max_num_paths] = val_rayt[i]['DoA_phi']
-    val_data[i,7+3*max_num_paths:7+4*max_num_paths] = val_rayt[i]['DoA_theta']
-    val_data[i,7+4*max_num_paths:7+5*max_num_paths] = val_rayt[i]['phase']
-    val_data[i,7+5*max_num_paths:7+6*max_num_paths] = val_rayt[i]['ToA']
-    val_data[i,7+6*max_num_paths:7+7*max_num_paths] = val_rayt[i]['power']
-    val_data[i,7+7*max_num_paths:7+8*max_num_paths] = val_rayt[i]['LoS']
-
-
-# CREATE VALIDATION DATASET
-length_test = len(test_idx)
-test_data = np.zeros([length_test,max_length])
-
-for i in range(int(length_test)):
-
-    test_data[i,0:2] = test_BS_loc[i,0:2]
-    test_data[i,2:4] = test_user_loc[i,0:2]
-    test_data[i,4] = test_user_dist[i]
-    test_data[i,5] = test_LoS[i]
-    test_data[i,6] = test_rayt[i]['num_paths']
-    test_data[i,7:7+max_num_paths] = test_rayt[i]['DoD_phi']
-    test_data[i,7+max_num_paths:7+2*max_num_paths] = test_rayt[i]['DoD_theta']
-    test_data[i,7+2*max_num_paths:7+3*max_num_paths] = test_rayt[i]['DoA_phi']
-    test_data[i,7+3*max_num_paths:7+4*max_num_paths] = test_rayt[i]['DoA_theta']
-    test_data[i,7+4*max_num_paths:7+5*max_num_paths] = test_rayt[i]['phase']
-    test_data[i,7+5*max_num_paths:7+6*max_num_paths] = test_rayt[i]['ToA']
-    test_data[i,7+6*max_num_paths:7+7*max_num_paths] = test_rayt[i]['power']
-    test_data[i,7+7*max_num_paths:7+8*max_num_paths] = test_rayt[i]['LoS']
 
 ## SAVE DATASETS
-
 training_data = {'training_data':training_data}
 val_data = {'val_data':val_data}
 test_data = {'test_data':test_data}
@@ -207,21 +166,8 @@ true_channels_training_data = {'true_channels_training_data':train_channels}
 true_channels_val_data = {'true_channels_val_data':val_channels}
 true_channels_test_data = {'true_channels_test_data':test_channels}
 
-# Save the datasets using h5py
-
-
-# Example: Saving data with hdf5storage
-matfiledata = {
-    "training_data": training_data,
-    "val_data": val_data,
-    "test_data": test_data,
-    "true_channels_training_data": true_channels_training_data,
-    "true_channels_val_data": true_channels_val_data,
-    "true_channels_test_data": true_channels_test_data,
-}
 
 # Save each dataset in a separate file
-
 hdf5storage.write(
     { "training_data": training_data },
     path=OUTPUT_DIR,
