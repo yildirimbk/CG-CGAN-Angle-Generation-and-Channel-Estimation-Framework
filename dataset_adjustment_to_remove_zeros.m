35576f2577f030233f02839662ce2af1a39d93b5
yildirimbk <124491344+yildirimbk@users.noreply.github.com>
2026-05-16 13:05:06 +0300

    Update dataset_adjustment_to_remove_zeros.m

--- dataset_adjustment_to_remove_zeros.m
+++ dataset_adjustment_to_remove_zeros.m
@@ -4,9 +4,11 @@ clear;
 close all;
 
 %Load Datasets
-training_dataset1 = load('training_dataset_4_path_80percent_k1.mat');
-val_dataset1 = load('validation_dataset_4_path_10percent_k1.mat');
-test_dataset1 = load('test_dataset_4_path_10percent_k1.mat');
+RUN_TAG = 'O1_60_Lp4_BS18_rows1-2751_TX8x4_RX4x2_sc1';
+
+training_dataset1 = load(['outputs/training_dataset_' RUN_TAG '.mat']);
+val_dataset1      = load(['outputs/validation_dataset_' RUN_TAG '.mat']);
+test_dataset1     = load(['outputs/test_dataset_' RUN_TAG '.mat']);
 
 training_dataset2 = struct2table(training_dataset1.training_data);
 training_dataset = table2array(training_dataset2);
@@ -18,7 +20,7 @@ test_dataset2 = struct2table(test_dataset1.test_data);
 test_dataset = table2array(test_dataset2);
 
 
-% Find no_path(-1) and remove them
+% Column 6 (1-indexed) is LoS status; -1 indicates no path | remove no path users
 nopath_ind_tr = find(training_dataset(:,6)==-1);
 nopath_ind_val = find(val_dataset(:,6)==-1);
 nopath_ind_test = find(test_dataset(:,6)==-1);
@@ -28,6 +30,6 @@ val_dataset(nopath_ind_val,:)=[];
 test_dataset(nopath_ind_test,:)=[];
 
 
-save('training_dataset_4_path_80percent_k1_no_path_removed.mat','training_dataset','-v7.3')
-save('validation_dataset_4_path_10percent_k1_no_path_removed.mat','val_dataset','-v7.3')
-save('test_dataset_4_path_10percent_k1_no_path_removed.mat','test_dataset','-v7.3')
+save(['outputs/training_dataset_' RUN_TAG '_no_path_removed.mat'], 'training_dataset', '-v7.3')
+save(['outputs/validation_dataset_' RUN_TAG '_no_path_removed.mat'], 'val_dataset', '-v7.3')
+save(['outputs/test_dataset_' RUN_TAG '_no_path_removed.mat'], 'test_dataset', '-v7.3')
